df6915c60a66eb73a6365e17b492849492311987
gc4342 <56233698+gc4342@users.noreply.github.com>
2019-12-03 20:41:49 -0500

    Revert "some comments"
    This reverts commit 16a3d21d36453cbfd04bfa7fac162851fade3109.

--- pathFinderRobot.m
+++ pathFinderRobot.m
@@ -14,7 +14,7 @@ classdef pathFinderRobot
         t3Up;
         t3Down;
         t1StartDrawingPos;
-        algorithm;
+        algorithm
         algorithm_choice;
         interpolation_method;
         mazefilename;
@@ -25,7 +25,7 @@ classdef pathFinderRobot
         
         start;
         goal;
-        
+
         %box variables
         box1Center;
         box2Center;
@@ -47,10 +47,21 @@ classdef pathFinderRobot
             
             % Initial servo positions
             rob.t1ZeroPosition = 0.13;       % theta1 actual zero position
-            rob.t2ZeroPosition = 0.7;        % theta2 actual zero position
+            rob.t2ZeroPosition = 0.7;          % theta2 actual zero position
             rob.t3Up = 140/180;              % theta3 up position
             rob.t3Down = 60/180;             % theta3 in down position
             rob.t1StartDrawingPos = 0.34;    % position for drawing purposes
+            
+            
+            %Box coordinate values
+            load mapvariable.mat;
+            box1Center = mymap(3,10); %top left box center point
+            box2Center = mymap(12,10); %top right box center point
+            box3Center = mymap(12,3); %bottom right box center point
+            box4Center = mymap(3,3); %bottom left box center point
+            centerBox = mymap(8,6); %Center box center point
+            
+
             
             rob.workFlag = 1;
             rob.checkFlag = 1;
@@ -64,18 +75,18 @@ classdef pathFinderRobot
             data = load('shapesinfo.txt');
             i = 1;
             writePosition(rob.servo3, rob.t3Up);
-            
+          
             while true % if file is empty > penup
                 if (data(i, 1) == 61.000000)
                     i = i+1;
                     writePosition(rob.servo3, rob.t3Up);
                 end
                 rob = checkPushButton(rob); % call to check if the button is pressed
-%                 if(rob.workFlag == 0)% || (rob.workFlag == 2))
-%                     disp('=========  Game Over  ============');
-%                     pause(20)
-%                     break;
-%                 end
+                if(rob.workFlag == 0)% || (rob.workFlag == 2))
+                    disp('=========  Game Over  ============');
+                    pause(20)
+                    break;
+                end
                 
                 q1 = data(i,1); % joint angle one
                 q2 = data(i,2); % joint angle two
@@ -97,6 +108,8 @@ classdef pathFinderRobot
         end
         
         function rob = drawPath(rob, start_x, start_y, blk_x, blk_y, reverse)
+            
+           
             load mapvariable.mat;
             if(blk_x ~= 0 || blk_y ~= 0)
                 fprintf('CAUTION: Road is blocked ahead at %d, %d\n', blk_x, blk_y);
@@ -109,18 +122,18 @@ classdef pathFinderRobot
                 rob.start = [start_x, start_y];
             end
             
-            rob.algorithm;
+            rob.algorithm;       
             switch rob.algorithm
-                case 'D_Star'
-                    disp('Dstar algorithm')
-                    ds=Dstar(mymap);
-                case 'PRM'
-                    disp('PRM algorithm')
-                    ds=PRM(mymap);
-                case 'DXForm'
-                    disp('DxForm algorithm')
-                    ds=DXform(mymap);
-                otherwise
+               case 'D_Star'
+                   disp('Dstar algorithm')
+                   ds=Dstar(mymap);
+               case 'PRM'
+                   disp('PRM algorithm')
+                   ds=PRM(mymap);
+               case 'DXForm'
+                   disp('DxForm algorithm')
+                   ds=DXform(mymap);
+               otherwise
                     disp('Not a valid option for algorithm')
             end
             ds.plan(rob.goal);
@@ -130,42 +143,43 @@ classdef pathFinderRobot
             [m,n]=size(path_points);
             rob.intp_method;
             switch rob.intp_method
-                case 'mtraj-tpoly'
-                    disp('Mtraj_tpoly')
+               case 'mtraj-tpoly'
+                   disp('Mtraj_tpoly')
+                   for i=1:1:m-1  %1 to 6
+                       if i==1
+                          mtraj_path_points_tpoly(1:10,1:2) = mtraj(@tpoly,[path_points(i,1) ...
+                          path_points(i,2)], [path_points(i+1,1) path_points(i+1,2)],10);
+                       else
+                          mtraj_path_points_tpoly((i*10)-9:10*i,1:2) = mtraj(@tpoly,[path_points(i,1) ...
+                          path_points(i,2)], [path_points(i+1,1) path_points(i+1,2)],10);                
+                       end
+                   end
+                   dlmwrite('mtrajPoints.txt',mtraj_path_points_tpoly,'delimiter','\t','newline','pc');
+                   rob.pathfilename = 'mtrajPoints.txt.txt';
+                   hold on;
+                   plot(mtraj_path_points_tpoly(:,1),mtraj_path_points_tpoly(:,2),'*w');
+                   pause(2);
+               case 'mtraj-lspb'
+                   disp('Mtraj_lspb')
                     for i=1:1:m-1  %1 to 6
-                        if i==1
-                            mtraj_path_points_tpoly(1:10,1:2) = mtraj(@tpoly,[path_points(i,1) ...
-                                path_points(i,2)], [path_points(i+1,1) path_points(i+1,2)],10);
-                        else
-                            mtraj_path_points_tpoly((i*10)-9:10*i,1:2) = mtraj(@tpoly,[path_points(i,1) ...
-                                path_points(i,2)], [path_points(i+1,1) path_points(i+1,2)],10);
-                        end
-                    end
-                    dlmwrite('mtrajPoints.txt',mtraj_path_points_tpoly,'delimiter','\t','newline','pc');
-                    rob.pathfilename = 'mtrajPoints.txt.txt';
-                    hold on;
-                    plot(mtraj_path_points_tpoly(:,1),mtraj_path_points_tpoly(:,2),'*w');
-                    pause(2);
-                case 'mtraj-lspb'
-                    disp('Mtraj_lspb')
-                    for i=1:1:m-1  %1 to 6 
-       %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%@Akshata, why is this 1 to 6??
-                        if i==1
-                            mtraj_path_points_lspb(1:10,1:2) = mtraj(@lspb,[path_points(i,1) ...
-                                path_points(i,2)], [path_points(i+1,1) path_points(i+1,2)],10);
-                        else
-                            mtraj_path_points_lspb((i*10)-9:10*i,1:2) = mtraj(@lspb,[path_points(i,1) ...
-                                path_points(i,2)], [path_points(i+1,1) path_points(i+1,2)],10);
-                        end
-                    end
-                    dlmwrite('mtrajPoints.txt.txt',mtraj_path_points_lspb,'delimiter','\t','newline','pc');
-                    rob.pathfilename = 'mtrajPoints.txt.txt';
-                    hold on;
-                    plot(mtraj_path_points_lspb(:,1),mtraj_path_points_lspb(:,2),'*w');
-                    pause(2);
-                otherwise
+                       if i==1
+                          mtraj_path_points_lspb(1:10,1:2) = mtraj(@lspb,[path_points(i,1) ...
+                          path_points(i,2)], [path_points(i+1,1) path_points(i+1,2)],10);
+                       else
+                          mtraj_path_points_lspb((i*10)-9:10*i,1:2) = mtraj(@lspb,[path_points(i,1) ...
+                          path_points(i,2)], [path_points(i+1,1) path_points(i+1,2)],10);                
+                       end
+                   end
+                   dlmwrite('mtrajPoints.txt.txt',mtraj_path_points_lspb,'delimiter','\t','newline','pc');
+                   rob.pathfilename = 'mtrajPoints.txt.txt';
+                   hold on;
+                   plot(mtraj_path_points_lspb(:,1),mtraj_path_points_lspb(:,2),'*w');
+                   pause(2);                  
+               otherwise
                     disp('Not a valid option for interpolation method')
             end
+            
+
         end
         
         function rob = checkPushButton(rob, new_x, new_y, blk_x, blk_y)
@@ -181,7 +195,8 @@ classdef pathFinderRobot
             if(pinValue2 == 1)
                 disp('==== Quitting maze solving and returning to start ====');
                 rob = drawPath(rob, fix(new_x), fix(new_y), 0, 0, 1);
-                rob.workFlag = 0;  
+                rob.workFlag = 0;
+                
             end
         end
         
@@ -206,17 +221,18 @@ classdef pathFinderRobot
             rob.intp_method = intp_method;
             switch algorithm
                 case 'D_Star'
-                    disp('Solving using D Star');
+%                     determine how to use algorithm
+                    disp('         Solving using D Star             ');
                     rob.algorithm = 1;
                     rob = drawPath(rob, rob.start,rob.goal, 0, 0, 0);
                     
                 case 'PRM'
-                    disp('Solving using PRM');
+%                     determine how to use algorithm
                     rob.algorithm = 2;
                     rob = drawPath(rob, rob.start,rob.goal, 0, 0, 0);
                     
                 case 'DXForm'
-                    disp('Solving using DXForm');
+%                     determine how to use algorithm
                     rob.algorithm = 3;
                     rob = drawPath(rob, rob.start,rob.goal, 0, 0, 0);
             end %Switch case
@@ -237,5 +253,6 @@ classdef pathFinderRobot
             clear all;
             clc;
         end
+        
     end % methods
 end % class def
